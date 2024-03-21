ca4f95394c700323a2e81b8e0a2e3afac76765d9
cangnss <cgunes52@gmail.com>
2024-03-21 20:56:16 +0300

    added customer's field validation

---
+++ src/validations/validateRequest.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import Joi, { number } from 'joi';
+
+export const validateRequest = (schema: Joi.Schema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+    next();
+  };
+};
+
+export const checkCustomerId = (req: Request, res: Response, next: NextFunction) => {
+  const { customerId } = req.params;
+  const convertedId = parseInt(customerId);
+  if (!customerId || isNaN(convertedId)) {
+    return res.status(400).json({ error: 'Invalid customerId' });
+  }
+  next();
+};
