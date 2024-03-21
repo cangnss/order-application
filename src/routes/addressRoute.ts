7c54454cc9042bb74f9445b679fd74e0d21c872a
cangnss <cgunes52@gmail.com>
2024-03-21 21:37:36 +0300

    created address routes, endpoints and validations

--- src/routes/addressRoute.ts
+++ src/routes/addressRoute.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { AddressController } from "../controllers/address.controller";
+import { createAddressSchema } from "../validations/addressSchema";
+import { checkAddressId, validateRequest } from "../validations/validateRequest";
 
 export const addressRoutes = Router();
 
 const addressControllers = new AddressController();
 
 addressRoutes.get("/", addressControllers.getAllAddress);
-addressRoutes.post("/", addressControllers.addAddress);
+addressRoutes.post("/", validateRequest(createAddressSchema), addressControllers.addAddress);
+addressRoutes.put("/:addressId", checkAddressId, validateRequest(createAddressSchema), addressControllers.updateAddress);
+addressRoutes.delete("/:addressId", checkAddressId, addressControllers.deleteAddress);
