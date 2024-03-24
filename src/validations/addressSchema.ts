f7b1b5c3e0884cf72fbe575e58a4ebaba6bf9752
cangnss <cgunes52@gmail.com>
2024-03-25 02:57:48 +0300

    added rabbitmq. created consumer and producer

--- src/validations/addressSchema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-export const createAddressSchema = Joi.object({
-    addressLine: Joi.string().required(),
-    city: Joi.string().required(),
-    country: Joi.string().required(),
-    cityCode: Joi.string().required(),
-    customer_id: Joi.number().required()
-});
--- src/validations/productSchema.ts
+++
@@ -1,6 +0,0 @@
-import Joi from 'joi';
-
-export const createProductSchema = Joi.object({
-    imageUrl: Joi.string().required(),
-    name: Joi.string().required()
-});
