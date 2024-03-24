408b61e32beca0d426a4a3c9291613e586839941
cangnss <cgunes52@gmail.com>
2024-03-24 17:08:56 +0300

    added winston, express-winston npm packages. created logger info, warning, error and internal error levels

---
+++ src/utils/logger.ts
@@ -0,0 +1,42 @@
+import { createLogger, format, transports } from "winston";
+
+
+export const logger = createLogger({
+    transports:[
+        new transports.Console(),
+        new transports.File({
+            level: "warn",
+            filename: "logsWarnings.log",
+            dirname: "src/logs"
+        }),
+        new transports.File({
+            level: "error",
+            filename: "logsErrors.log",
+            dirname: "src/logs"
+        }),
+        new transports.File({
+            level: "info",
+            filename: "logsInfo.log",
+            dirname: "src/logs"
+        }),
+    ],
+    format: format.combine(
+        format.json(),
+        format.timestamp(),
+        format.prettyPrint()
+    ),
+})
+
+export const internalErrorLogger = createLogger({
+    transports:[
+        new transports.File({
+            filename: "logsInternalErrors.log",
+            dirname: "src/logs"
+        }),
+    ],
+    format: format.combine(
+        format.json(),
+        format.timestamp(),
+        format.prettyPrint()
+    ),
+})
